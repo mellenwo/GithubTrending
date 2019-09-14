ca6c2a621e17a64c53f185cbd3c8e797c68ff9c9
mellenwo <mellenwo@villanova.edu>
2019-09-13 21:49:53 -0700

    creating the cached project DAO

---
+++ cache/src/main/java/com/mellenwood/cache/dao/CachedProjectsDao.kt
@@ -0,0 +1,34 @@
+package com.mellenwood.cache.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.mellenwood.cache.db.ProjectConstants
+import com.mellenwood.cache.db.ProjectConstants.DELETE_PROJECTS
+import com.mellenwood.cache.db.ProjectConstants.QUERY_BOOKMARKED_PROJECTS
+import com.mellenwood.cache.db.ProjectConstants.QUERY_PROJECTS
+import com.mellenwood.cache.db.ProjectConstants.QUERY_UPDATE_BOOKMARK_STATUS
+import com.mellenwood.cache.model.CachedProject
+import io.reactivex.Flowable
+
+@Dao
+abstract class CachedProjectsDao {
+
+    @Query(QUERY_PROJECTS)
+    abstract fun getProjects(): Flowable<List<CachedProject>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    abstract fun insertProjects(projects: List<CachedProject>)
+
+    @Query(DELETE_PROJECTS)
+    abstract fun deleteProjects()
+
+    @Query(QUERY_BOOKMARKED_PROJECTS)
+    abstract fun getBookmarkedProjects(): Flowable<List<CachedProject>>
+
+    @Query(QUERY_UPDATE_BOOKMARK_STATUS)
+    abstract fun setBookmarkStatus(isBookmarked: Boolean,
+                                   projectId: String)
+
+}
--- cache/src/main/java/com/mellenwood/cache/model/CachedProject.kt
+++ cache/src/main/java/com/mellenwood/cache/model/CachedProject.kt
@@ -1,5 +1,6 @@
 package com.mellenwood.cache.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.mellenwood.cache.db.ProjectConstants
@@ -7,6 +8,7 @@ import com.mellenwood.cache.db.ProjectConstants
 @Entity(tableName = ProjectConstants.TABLE_NAME)
 class CachedProject (
         @PrimaryKey
+        @ColumnInfo(name = ProjectConstants.COLUMN_PROJECT_ID)
         var id: String,
         var name: String,
         var fullName: String,
@@ -14,5 +16,6 @@ class CachedProject (
         var dateCreated: String,
         var ownerName: String,
         var ownerAvatar: String,
+        @ColumnInfo(name = ProjectConstants.COLUMN_IS_BOOKMARKED)
         var isBookmarked: Boolean
 )
