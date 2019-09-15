61b45a9bc341c0c9f665d917f2c2bf01b6fb1649
mellenwo <mellenwo@villanova.edu>
2019-09-14 20:35:51 -0700

    fixed broken tests

--- cache/src/main/java/com/mellenwood/cache/model/CachedProject.kt
+++ cache/src/main/java/com/mellenwood/cache/model/CachedProject.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 import com.mellenwood.cache.db.ProjectConstants
 
 @Entity(tableName = ProjectConstants.TABLE_NAME)
-class CachedProject (
+data class CachedProject (
         @PrimaryKey
         @ColumnInfo(name = ProjectConstants.COLUMN_PROJECT_ID)
         var id: String,
@@ -18,16 +18,4 @@ class CachedProject (
         var ownerAvatar: String,
         @ColumnInfo(name = ProjectConstants.COLUMN_IS_BOOKMARKED)
         var isBookmarked: Boolean
-) {
-        override fun equals(other: Any?): Boolean {
-                return this.id.equals((other as CachedProject).id)
-                        && this.name.equals(other.name)
-                        && this.dateCreated.equals(other.dateCreated)
-                        && this.fullName.equals(other.fullName)
-                        && this.ownerAvatar.equals(other.ownerAvatar)
-                        && this.ownerName.equals(other.ownerName)
-                        && this.isBookmarked.equals(other.isBookmarked)
-                        && this.starCount.equals(other.starCount)
-
-        }
-}
+)
