6f843dcbc27f44d8465e5d9acdcb42d73b704005
mellenwo <mellenwo@villanova.edu>
2019-09-15 12:50:32 -0700

    creating the presentation state model

---
+++ Presentation/src/main/java/com/mellenwood/presentation/state/Resource.kt
@@ -0,0 +1,9 @@
+package com.mellenwood.presentation.state
+
+class Resource<out T> constructor(val status: ResourceState,
+                                  val data: T?,
+                                  val message: String?) {
+
+    
+
+}
---
+++ Presentation/src/main/java/com/mellenwood/presentation/state/ResourceState.kt
@@ -0,0 +1,5 @@
+package com.mellenwood.presentation.state
+
+enum class ResourceState {
+    LOADING, SUCCESS, ERROR
+}
