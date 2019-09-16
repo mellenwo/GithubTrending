3224049aa3e4ac95ea2748e1bdb3f3fa2d0d47ec
mellenwo <mellenwo@villanova.edu>
2019-09-15 19:03:36 -0700

    testing browse bookmarked projects view model implementation

---
+++ Presentation/src/test/java/com/mellenwood/presentation/bookmarked/BrowseBookmarkedProjectsViewModelTest.kt
@@ -0,0 +1,98 @@
+package com.mellenwood.presentation.bookmarked
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.mellenwood.domain.interactor.bookmarked.GetBookmarkedProjects
+import com.mellenwood.domain.model.Project
+import com.mellenwood.presentation.BrowseBookmarkedProjectsViewModel
+import com.mellenwood.presentation.mapper.ProjectViewMapper
+import com.mellenwood.presentation.model.ProjectView
+import com.mellenwood.presentation.state.ResourceState
+import com.mellenwood.presentation.test.factory.DataFactory
+import com.mellenwood.presentation.test.factory.ProjectFactory
+import com.nhaarman.mockitokotlin2.*
+import io.reactivex.observers.DisposableObserver
+import junit.framework.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Captor
+
+class BrowseBookmarkedProjectsViewModelTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+    var getBookmarkedProjects = mock<GetBookmarkedProjects>()
+    var mapper = mock<ProjectViewMapper>()
+    var projectViewModel = BrowseBookmarkedProjectsViewModel(getBookmarkedProjects, mapper)
+
+    @Captor
+    val captor = argumentCaptor<DisposableObserver<List<Project>>>()
+
+    @Test
+    fun fetchProjectsExecutesUseCase() {
+        projectViewModel.fetchProjects()
+
+        verify(getBookmarkedProjects, times(1)).execute(any(), eq(null))
+    }
+
+    @Test
+    fun fetchProjectsReturnsSuccess() {
+        val projects = ProjectFactory.makeProjectList(2)
+        val projectViews = ProjectFactory.makeProjectViewList(2)
+        stubProjectMapperMapToView(projectViews[0], projects[0])
+        stubProjectMapperMapToView(projectViews[1], projects[1])
+
+        projectViewModel.fetchProjects()
+
+        verify(getBookmarkedProjects).execute(captor.capture(), eq(null))
+        captor.firstValue.onNext(projects)
+
+        assertEquals(
+            ResourceState.SUCCESS,
+            projectViewModel.getProjects().value?.status)
+    }
+
+    @Test
+    fun fetchProjectsReturnsData() {
+        val projects = ProjectFactory.makeProjectList(2)
+        val projectViews = ProjectFactory.makeProjectViewList(2)
+        stubProjectMapperMapToView(projectViews[0], projects[0])
+        stubProjectMapperMapToView(projectViews[1], projects[1])
+
+        projectViewModel.fetchProjects()
+
+        verify(getBookmarkedProjects).execute(captor.capture(), eq(null))
+        captor.firstValue.onNext(projects)
+
+        assertEquals(projectViews,
+            projectViewModel.getProjects().value?.data)
+    }
+
+    @Test
+    fun fetchProjectsReturnsError() {
+        projectViewModel.fetchProjects()
+
+        verify(getBookmarkedProjects).execute(captor.capture(), eq(null))
+        captor.firstValue.onError(RuntimeException())
+
+        assertEquals(ResourceState.ERROR,
+            projectViewModel.getProjects().value?.status)
+    }
+
+    @Test
+    fun fetchProjectsReturnsMessageForError() {
+        val errorMessage = DataFactory.randomString()
+        projectViewModel.fetchProjects()
+
+        verify(getBookmarkedProjects).execute(captor.capture(), eq(null))
+        captor.firstValue.onError(RuntimeException(errorMessage))
+
+        assertEquals(errorMessage,
+            projectViewModel.getProjects().value?.message)
+    }
+
+    private fun stubProjectMapperMapToView(projectView: ProjectView,
+                                           project: Project) {
+        whenever(mapper.mapToView(project))
+            .thenReturn(projectView)
+    }
+}
