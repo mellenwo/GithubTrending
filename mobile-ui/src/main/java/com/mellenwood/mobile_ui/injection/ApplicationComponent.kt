b1e46d349d8ead1bb62fc5b4a667ecf245b50c2b
mellenwo <mellenwo@villanova.edu>
2019-09-16 17:54:49 -0700

    setting up the dependency injection for the ui module

---
+++ mobile-ui/src/main/java/com/mellenwood/mobile_ui/injection/ApplicationComponent.kt
@@ -0,0 +1,24 @@
+package com.mellenwood.mobile_ui.injection
+
+import android.app.Application
+import com.mellenwood.mobile_ui.GithubTrendingApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = arrayOf(AndroidInjectionModule::class))
+interface ApplicationComponent {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): ApplicationComponent
+    }
+
+    fun inject(app: GithubTrendingApplication)
+
+}
