a96f04b205f717913e10eb965859de982c3edab9
mellenwo <mellenwo@villanova.edu>
2019-09-16 18:00:05 -0700

    creating the UI modules Project representation

---
+++ mobile-ui/src/main/java/com/mellenwood/mobile_ui/model/Project.kt
@@ -0,0 +1,6 @@
+package com.mellenwood.mobile_ui.model
+
+data class Project(val id: String, val name: String, val fullName: String,
+              val starCount: String, val dateCreated: String,
+              val ownerName: String, val ownerAvatar: String,
+              val isBookmarked: Boolean)
